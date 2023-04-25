70c8a8fc8f8b35a8163a84b7831e7a765f0ca572
Pau Matas <pmatasalbiol@gmail.com>
2023-04-25 17:46:38 +0200

    feat: matchady graph implemented

--- src/graphs/matchday_graph.py
+++ src/graphs/matchday_graph.py
@@ -1,18 +1,46 @@
-import networkx as nx
-from datetime import timedelta
+from networkx import DiGraph
+from datetime import datetime, timedelta
 
 from src.interfaces.scraper import run_matches_spider
+from src.interfaces.database import MongoDBDatabaseProxy
 from src.routes import OpenRouteServiceProxy
 
 class MatchDayGraph:
-    def __init__(self):
-        # acess the next matchdays for the different competitions and get the matches
-        self._matches = run_matches_spider()
-        self._matches = sorted(self._matches, key=lambda x: x['timestamp'])
+    def __init__(self, date: datetime | str = None, **kwargs):
+        if isinstance(date, datetime):
+            self.date = date
+        elif isinstance(date, str):
+            self.date = datetime.strptime(date, kwargs.get('format', '%d-%m-%Y'))
+        elif date is None:
+            if ['day', 'month', 'year'] in kwargs:
+                self.date = datetime(kwargs['year'], kwargs['month'], kwargs['day'])
+            else:
+                now = datetime.now()
+                today = datetime(now.year, now.month, now.day) # set time to 00:00:00
+                saturday = today + timedelta(days=5-today.weekday())
+                sunday = today + timedelta(days=6-today.weekday())
+                if today.weekday() == 5:
+                    self.date = sunday
+                elif today.weekday() == 6:
+                    self.date = today + timedelta(days=6)
+                else:
+                    self.date = saturday
 
-        # add the matches as nodes, with a weight parameter that will be defined later and the edges between the matches
-        # if it is possible to go from one match to the other and add the time difference in minutes as a weight parameter
-        self._matches_to_graph()
+        if (graph := MongoDBDatabaseProxy().get_matchday_graph(self.date)) is not None:
+            self.graph = graph
+        else:
+            # acess the next matchdays for the different competitions and get the matches
+            self._get_matches()
+            # add the matches as nodes, with a weight parameter that will be defined later and the edges between the matches
+            # if it is possible to go from one match to the other and add the time difference in minutes as a weight parameter
+            self._matches_to_graph()
+            # save the graph in the database
+            MongoDBDatabaseProxy().save_matchday_graph(self.date, self.graph)
+
+    def _get_matches(self):
+        self._matches = run_matches_spider()
+        self._matches = list(filter(lambda x: 'latlon' in x and 'timestamp' in x, self._matches))
+        self._matches = sorted(self._matches, key=lambda x: x['timestamp'])
 
     def node_weight(self, match: list[dict]):
         # TODO: define the weight of the node
@@ -40,7 +68,7 @@ class MatchDayGraph:
                         weight=(destination_match['timestamp'] - origin_match['timestamp']).total_seconds() / 60.0)
 
     def _matches_to_graph(self):
-        self.graph = nx.Graph()
+        self.graph = DiGraph()
 
         self._add_nodes()
         self._add_edges()
